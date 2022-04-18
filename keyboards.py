eace17bc13adc9631e89859fd8a18c7b7f187888
Андрей Волков <andrey.dreamwall@gmail.com>
2022-04-18 09:25:30 +0300

    create Keyboard class for all keyboards

--- keyboards.py
+++ keyboards.py
@@ -3,24 +3,47 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from typing import List
 
 
-class MainMenu:
-    def __init__(self):
+class Keyboard:
+    def __init__(self, button_names: List[str]):
+        self.button_names = button_names
         self.resize_keyboard: bool = True
-        self.one_time_keyboard: bool = False
+        self.one_time_keyboard: bool = True
         self.row_width: int = 2
-        self.button4 = KeyboardButton(text='Help')
-        self.button3 = KeyboardButton(text='Stats')
-        self.button2 = KeyboardButton(text='Income')
-        self.button1 = KeyboardButton(text='Expense')
 
-    def buttons(self) -> List:
-        button_list = [self.button1, self.button2,
-                       self.button3, self.button4]
-        return list(button_list)
+    def create_buttons(self) -> List[KeyboardButton]:
+        buttons_list = []
+        for button_name in self.button_names:
+            buttons_list.append(KeyboardButton(text=button_name))
+        return buttons_list
 
-    def keyboard(self) -> ReplyKeyboardMarkup:
-        keys_list = self.buttons()
-        keys = ReplyKeyboardMarkup(
+    def create_keyboard(self) -> ReplyKeyboardMarkup:
+        keys_list = self.create_buttons()
+        keyboard = ReplyKeyboardMarkup(
             row_width=self.row_width,
-            resize_keyboard=self.resize_keyboard).add(*keys_list)
-        return keys
+            resize_keyboard=self.resize_keyboard,
+            one_time_keyboard=self.one_time_keyboard).add(*keys_list)
+        return keyboard
+
+
+class MainMenu(Keyboard):
+    def __init__(self):
+        button_names = ['Expense', 'Income', 'Amount', 'Stats']
+        super().__init__(button_names)
+
+
+class ExpenseCategories(Keyboard):
+    def __init__(self):
+        button_names = ['Home', 'Groceries', 'Restaurants',
+                        'Sport', 'Clothes', 'Travel']
+        super().__init__(self, button_names)
+
+
+class HomeSubcategories(Keyboard):
+    def __init__(self):
+        button_names = ['Water', 'Electricity', 'Internet', 'Chemicals',
+                        'Medicine', 'Subscriptions', 'Other-Home']
+        super().__init__(self, button_names)
+
+
+class GroceriesSubcategories(Keyboard):
+    button_names = ['Meat',]
