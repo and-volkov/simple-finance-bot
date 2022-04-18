57df1cd2e95a99bb67ebb235e0293d6837cd7629
Андрей Волков <andrey.dreamwall@gmail.com>
2022-04-18 09:25:01 +0300

    create get_columns, _init_db, check_db_exists functions

--- db_queries.py
+++ db_queries.py
@@ -7,3 +7,28 @@ import os
 connection = sqlite3.connect(os.path.join('db', 'finances.db'))
 cursor = connection.cursor()
 
+
+def get_columns(table_name: str, name) -> List[str]:
+    cursor.execute(f'SELECT {name} FROM {table_name}')
+    cols = cursor.fetchall()
+    return cols
+
+
+def _init_db():
+    """Инициализирует БД"""
+    with open("db/createdb.sql", "r") as f:
+        sql = f.read()
+    cursor.executescript(sql)
+    connection.commit()
+
+
+def check_db_exists():
+    """Проверяет, инициализирована ли БД, если нет — инициализирует"""
+    cursor.execute('SELECT name FROM sqlite_master '
+                   'WHERE type=\'table\' AND name=\'categories\'')
+    table_exists = cursor.fetchall()
+    if not table_exists:
+        _init_db()
+
+
+check_db_exists()
