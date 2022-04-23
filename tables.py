5a30676afb89cf5bdf09b624de6e36b771d2105f
Андрей Волков <andrey.dreamwall@gmail.com>
2022-04-23 14:07:35 +0300

    create GraphicStatistic class

--- tables.py
+++ tables.py
@@ -8,19 +8,30 @@ import matplotlib.colors as mcolors
 import db_queries
 
 
-graphs_folder = os.path.dirname('graphs')
+class GraphStatistic:
+    def __init__(self):
+        self.action = 'pandas'
+        self.graph_folder = os.path.dirname('graphs/')
+        self.queries_dict = {
+            'GraphToday': db_queries.get_today_stats(self.action),
+            'GraphWeek': db_queries.get_weekly_stats(self.action),
+            'GraphMonth': db_queries.get_monthly_stats(self.action),
+            'GraphAllTime': db_queries.get_top_ten_stats(self.action)
+        }
 
-connection = sqlite3.connect(os.path.join('db', 'finances.db'))
-cursor = connection.cursor()
+        self.connection = sqlite3.connect(os.path.join('db', 'finances.db'))
 
-query = db_queries.get_monthly_stats(action='pandas')
-df = pd.read_sql(query, connection)
+    def create_plot(self, query_name):
+        df = pd.read_sql(
+            self.queries_dict[query_name],
+            self.connection
+        )
 
-plt.figure(figsize=(10, 7))
-plt.grid(True)
-plt.title('expenses')
-plt.barh(
-    df['subcategorie'], df['summary'],
-    height=0.7,
-    color=mcolors.TABLEAU_COLORS)
-plt.show()
+        fig = plt.figure(figsize=(8, 4))
+        plt.grid(True)
+        plt.title('expenses')
+        plt.barh(
+            df['subcategorie'], df['summary'],
+            height=0.7,
+            color=mcolors.TABLEAU_COLORS)
+        fig.savefig(os.path.join(self.graph_folder, 'output.png'))
